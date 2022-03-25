fbbadb17831383ab03ab73f25abc024df89efc2e
fanik05 <fatinanik129@gmail.com>
2022-03-25 10:49:36 +0600

    unit tests added

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'linebreak-style': ['error', 'windows'],
     quotes: ['error', 'single'],
     semi: ['error', 'never'],
+    eqeqeq: 'error',
     'no-trailing-spaces': 'error',
     'object-curly-spacing': ['error', 'always'],
     'arrow-spacing': ['error', { before: true, after: true }],
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,9 @@
-const dummy = blogs => {
-  return 1
+const totalLikes = blogs => {
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
 module.exports = {
-  dummy,
+  totalLikes,
 }
