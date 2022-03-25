09801f3480a129ad540b91f2f78bfce899355893
fanik05 <fatinanik129@gmail.com>
2022-03-25 15:43:33 +0600

    unit demo tests added

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -5,15 +5,51 @@ const totalLikes = blogs => {
 }
 
 const favoriteBlog = blogs => {
-  if (blogs.length === 0) return {}
+  const blogLikes = blogs.map(blog => blog.likes)
+  const maxLikes = Math.max(...blogLikes)
+  const { title, author, likes } = blogs.filter(
+    blog => blog.likes === maxLikes
+  )[0]
 
-  const likes = blogs.map(blog => blog.likes)
-  const maxLikes = Math.max(...likes)
+  return { title, author, likes }
+}
 
-  return blogs.filter(blog => blog.likes === maxLikes)[0]
+const mostBlogs = blogs => {
+  const authors = [...new Set(blogs.map(blog => blog.author))]
+  const blogNo = []
+  authors.forEach(author => {
+    blogNo.push(blogs.filter(blog => blog.author === author).length)
+  })
+  const maxBlog = Math.max(...blogNo)
+
+  return {
+    author: authors[blogNo.indexOf(maxBlog)],
+    blogs: maxBlog,
+  }
+}
+
+const mostLikes = blogs => {
+  const authors = [...new Set(blogs.map(blog => blog.author))]
+  const bloglikes = []
+  authors.forEach(author => {
+    bloglikes.push(
+      blogs
+        .filter(blog => blog.author === author)
+        .map(blog => blog.likes)
+        .reduce((sum, likes) => sum + likes, 0)
+    )
+  })
+  const maxLikes = Math.max(...bloglikes)
+
+  return {
+    author: authors[bloglikes.indexOf(maxLikes)],
+    blogs: maxLikes,
+  }
 }
 
 module.exports = {
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 }
